e5878294422c667b3ff87e03cc31221e7dff276e
Alexis_franco <brandonalexisfrancof@gmail.com>
2025-07-14 20:48:38 -0500

    Create AvatarInnovador.h

---
+++ AvatarInnovador.h
@@ -0,0 +1,11 @@
+#ifndef AVATARINNOVADOR_H
+#define AVATARINNOVADOR_H
+
+#include "Avatar.h"
+
+class AvatarInnovador : public Avatar {
+public:
+    AvatarInnovador();
+};
+
+#endif // AVATARINNOVADOR_H
